0cfd085987a9adfcca5bc793c92796d4e66b2732
ahmedahshlw <ahmedit5216@gmail.com>
2024-08-07 09:25:59 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello!!</h1>
+      <h3>come everyday</h3>
+    </>
+  );
+}
+
+export default App;
